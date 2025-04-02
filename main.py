e63e79821d79149116452d2f2023ed1daaad3889
Homero Pytlovanciw <homero.pytlovanciw@digiaccess.com.br>
2025-04-02 11:16:50 -0300

    refactor (main.py): Variáveis refatoradas para trabalhar com as classes de forma correta.

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
-from hill import hill_encrypt, hill_decrypt
-from vigenere import vigenere_encrypt, vigenere_decrypt
+import numpy as np
+from hill import Hill
+from vigenere import Vigenere
 
 def testar_algoritmos():
-    texto_original = "testandoalgoritmos"
+    texto_original = "hello"
     chave_vigenere = "bruh"
+
+    matriz = np.array([[3,3], [2,5]])
+
+    hill = Hill(matriz)
+    vigenere = Vigenere(chave_vigenere)
     
     print("Texto original:", texto_original)
     
-    texto_cifrado_hill = hill_encrypt(texto_original)
+    texto_cifrado_hill = hill.hill_encrypt(texto_original)
     print("\nCifra de Hill:")
     print("Texto cifrado:", texto_cifrado_hill)
-    texto_decifrado_hill = hill_decrypt(texto_cifrado_hill)
+    texto_decifrado_hill = hill.hill_decrypt(texto_cifrado_hill)
     print("Texto decifrado:", texto_decifrado_hill)
     
-    texto_cifrado_vigenere = vigenere_encrypt(texto_original, chave_vigenere)
+    texto_cifrado_vigenere = vigenere.vigenere_encrypt(texto_original)
     print("\nCifra de Vigenère (chave:", chave_vigenere, "):")
     print("Texto cifrado:", texto_cifrado_vigenere)
-    texto_decifrado_vigenere = vigenere_decrypt(texto_cifrado_vigenere, chave_vigenere)
+    texto_decifrado_vigenere = vigenere.vigenere_decrypt(texto_cifrado_vigenere)
     print("Texto decifrado:", texto_decifrado_vigenere)
 
 if __name__ == "__main__":
